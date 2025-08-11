859e67b62c8bdf29eeb43681c0e221ddd75e7db8
zxmlysxl <zxmlysxl@gmail.com>
2025-08-11 22:05:06 +0800

    nft-timecontrol降级至2.0.2

--- luci-app-nft-timecontrol/luasrc/model/cbi/timecontrol.lua
+++ luci-app-nft-timecontrol/luasrc/model/cbi/timecontrol.lua
@@ -20,14 +20,14 @@ e.value = translate("Collecting data...")
 e = t:option(ListValue, "list_type",translate("Control mode"), translate("blacklist:Block the networking of the target address,whitelist:Only allow networking for the target address and block all other addresses."))
 e.rmempty = false
 e:value("blacklist", translate("blacklist"))
--- e:value("whitelist", translate("whitelist"))
+e:value("whitelist", translate("whitelist"))
 e.default = "blacklist"
 
 e = t:option(ListValue, "chain",translate("Control intensity"), translate("Pay attention to strong control: machines under control will not be able to connect to the software router backend!"))
 e.rmempty = false
--- e:value("forward", translate("Ordinary forwarding control"))
+e:value("forward", translate("Ordinary forwarding control"))
 e:value("input", translate("Strong inbound control"))
-e.default = "input"
+e.default = "forward"
 
 t = a:section(TypedSection, "device")
 t.template = "cbi/tblsection"
@@ -42,104 +42,31 @@ e.rmempty = false
 e.size = 4
 
 ip = t:option(Value, "mac", translate("IP/MAC"))
-ip.size = 8
 
--- 替换原有的 get_devices() 函数
-local function get_devices()
-    local devices = {}
-    local seen_ips = {}
-    local ubus = require "ubus"
-    local conn = ubus.connect()
-    
-    -- 辅助函数：尝试获取主机名
-    local function get_hostname(ip)
-        -- 方法1: 使用 nslookup
-        local f = io.popen("nslookup "..ip.." 2>/dev/null | grep 'name =' | cut -d'=' -f2 | sed 's/\\.$//'")
-        if f then
-            local name = f:read("*l")
-            f:close()
-            if name and name ~= "" then
-                return name:match("^%s*(.-)%s*$")  -- 去除前后空格
-            end
-        end
-        
-        -- 方法2: 读取 /tmp/dhcp.leases
-        local leases_file = io.open("/tmp/dhcp.leases", "r")
-        if leases_file then
-            for line in leases_file:lines() do
-                local mac, ip_lease, _, hostname = line:match("^(%S+)%s+(%S+)%s+(%S+)%s+(%S+)")
-                if ip_lease == ip and hostname ~= "*" then
-                    leases_file:close()
-                    return hostname
-                end
-            end
-            leases_file:close()
-        end
-        
-        return "unknown"
-    end
+ipc.neighbors({family = 4, dev = "br-lan"}, function(n)
+	if n.mac and n.dest then
+		ip:value(n.dest:string(), "%s (%s)" %{ n.dest:string(), n.mac })
+	end
+end)
+ipc.neighbors({family = 4, dev = "br-lan"}, function(n)
+	if n.mac and n.dest then
+		ip:value(n.mac, "%s (%s)" %{n.mac, n.dest:string() })
+	end
+end)
 
-    -- 1. 从DHCP租约获取设备
-    if conn then
-        local leases = conn:call("dhcp", "ipv4leases", {}) or {}
-        for _, lease in ipairs(leases) do
-            if lease.ipaddr and lease.mac then
-                local hostname = lease.hostname or get_hostname(lease.ipaddr)
-                devices[#devices+1] = {
-                    ip = lease.ipaddr,
-                    mac = lease.mac:upper(),  -- 统一转为大写
-                    hostname = hostname,
-                    display = string.format("%s (%s) - %s", lease.ipaddr, lease.mac:upper(), hostname)
-                }
-                seen_ips[lease.ipaddr] = true
-            end
-        end
-        conn:close()
-    end
+e.size = 8
 
-    -- 2. 从ARP表获取设备（使用ip neigh命令）
-    local arp_cmd = io.popen("ip -4 neigh show dev br-lan 2>/dev/null")
-    if arp_cmd then
-        for line in arp_cmd:lines() do
-            local ip_addr, mac = line:match("^(%S+)%s+.+%s+(%S+)%s+")
-            if ip_addr and mac and mac ~= "00:00:00:00:00:00" and not seen_ips[ip_addr] then
-                mac = mac:upper()  -- 统一MAC地址格式
-                local hostname = get_hostname(ip_addr)
-                devices[#devices+1] = {
-                    ip = ip_addr,
-                    mac = mac,
-                    hostname = hostname,
-                    display = string.format("%s (%s) - %s", ip_addr, mac, hostname)
-                }
-                seen_ips[ip_addr] = true
-            end
-        end
-        arp_cmd:close()
-    end
-
-    -- 按IP地址排序设备列表
-    table.sort(devices, function(a, b) return a.ip < b.ip end)
-    
-    return devices
-end
-
-
--- 添加设备选项
-local devices = get_devices()
-for _, dev in ipairs(devices) do
-    ip:value(dev.ip, dev.display)
-end
 
 function validate_time(self, value, section)
-    local hh, mm, ss
-    hh, mm, ss = string.match(value, "^(%d?%d):(%d%d)$")
-    hh = tonumber(hh)
-    mm = tonumber(mm)
-    if hh and mm and hh <= 23 and mm <= 59 then
-        return value
-    else
-        return nil, "Time HH:MM or space"
-    end
+        local hh, mm, ss
+        hh, mm, ss = string.match (value, "^(%d?%d):(%d%d)$")
+        hh = tonumber (hh)
+        mm = tonumber (mm)
+        if hh and mm and hh <= 23 and mm <= 59 then
+            return value
+        else
+            return nil, "Time HH:MM or space"
+        end
 end
 
 e = t:option(Value, "timestart", translate("Start control time"))
